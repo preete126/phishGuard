229a5e69f64a6566c4dc1e366fbe05265b5d5167
Ismail Mubarokah <ismailbarakatopeyemi05@gmail.com>
2025-02-04 16:13:27 +0100

    chore: enhanced the UI on smaller screens

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,14 +1,14 @@
 <template>
     <v-main class="bg pa-3">
-        <div class="d-flex ga-3 align-center">
-            <h1 class="text-h3 text-sm-h2 font-weight-black">PhishGuard</h1>
-            <v-icon size="50">mdi-shield-check</v-icon>
+        <div class="d-flex ga-1 ga-sm-3 align-center">
+            <h1 class="text-h4 text-sm-h2 font-weight-black">PhishGuard</h1>
+            <v-icon class="text-h4 text-sm-h2">mdi-shield-check</v-icon>
         </div>
-        <p class="mx-auto mb-5 text-h6 w-sm-75 text-center">Stay Safe Online – Instantly Detect Malicious and Phishing
+        <p class="mx-auto mb-5 text-sm-h6 w-sm-75 text-center">Stay Safe Online – Instantly Detect Malicious and Phishing
             Links</p>
         <v-card class="py-5 px-2 pa-sm-7 w-100 w-md-75">
             <!-- <v-card-item> -->
-                <v-card-title class="text-center mb-2 text-wrap">Check If a URL is Safe or a Phishing
+                <v-card-title class="text-center mb-2 text-wrap text-subtitle-1 text-sm-h6">Check If a URL is Safe or a Phishing
                     Trap</v-card-title>
                 <v-form v-model="form" @submit.prevent="scanUrl" class="d-flex align-center mb-4" style="height: 3em;">
                     <v-text-field v-model="url" :rules="[urlValidation]" class="h-100"
@@ -20,51 +20,51 @@
     </v-main>
 
     <v-main class=" w-100 layout pa-5 mx-auto mb-15">
-        <h2 class="text-center mb-10 mb-sm-15 text-h5 text-sm-h4 font-weight-black">Common Types of Dangerous Links You
+        <h2 class="text-center mb-10 mb-sm-15 text-h6 text-sm-h4 font-weight-black">Common Types of Dangerous Links You
             Should Avoid</h2>
         <v-row class="gap">
-            <v-col cols="12" sm="" class="bg-surface pa-5 rounded-lg">
-                <v-icon color="red-lighten-3" size="40">mdi-web</v-icon>
-                <p class="mt-5 font-weight-black text-h6">Deceptive Websites</p>
+            <v-col cols="12" sm="" class="bg-surface pa-3 pa-sm-5 text-justify rounded-lg">
+                <v-icon color="red-lighten-3" class="text-h5 text-sm-h4" >mdi-web</v-icon>
+                <p class="mt-5 font-weight-black text-subtitle-1 text-sm-h6">Deceptive Websites</p>
                 <p class="mt-5">These are fake websites designed to look like legitimate ones. They trick users into
                     entering sensitive information like passwords and credit card details. Always verify the domain name
                     before entering any credentials.</p>
             </v-col>
-            <v-col cols="12" sm="" class="bg-surface pa-5 rounded-lg">
-                <v-icon color="red-lighten-3" size="40">mdi-link-variant-remove</v-icon>
-                <p class="mt-5 font-weight-black text-h6">Suspicious Shortened Links</p>
+            <v-col cols="12" sm="" class="bg-surface pa-3 pa-sm-5 text-justify rounded-lg">
+                <v-icon color="red-lighten-3" class="text-h5 text-sm-h4" >mdi-link-variant-remove</v-icon>
+                <p class="mt-5 font-weight-black text-subtitle-1 text-sm-h6">Suspicious Shortened Links</p>
                 <p class="mt-5">URL shorteners hide the real destination of a link, making it easy for cybercriminals to
                     disguise malicious sites. Use URL preview tools to check where the link leads before clicking.</p>
             </v-col>
         </v-row>
         <v-row class="gap my-10">
-            <v-col cols="12" sm="" class="bg-surface pa-5 rounded-lg">
-                <v-icon color="red-lighten-3" size="40">mdi-login-variant</v-icon>
-                <p class="mt-5 font-weight-black text-h6">Fake Login Pages</p>
+            <v-col cols="12" sm="" class="bg-surface pa-3 pa-sm-5 text-justify rounded-lg">
+                <v-icon color="red-lighten-3" class="text-h5 text-sm-h4">mdi-login-variant</v-icon>
+                <p class="mt-5 font-weight-black text-subtitle-1 text-sm-h6">Fake Login Pages</p>
                 <p class="mt-5">These pages mimic real websites and ask for login credentials. Once you enter them,
                     hackers gain access to your accounts. Always check the website’s URL and use two-factor
                     authentication for extra security.</p>
             </v-col>
-            <v-col cols="12" sm="" class="bg-surface pa-5 rounded-lg">
-                <v-icon color="red-lighten-3" size="40">mdi-bug-outline</v-icon>
-                <p class="mt-5 font-weight-black text-h6">Malware-Infested Links</p>
+            <v-col cols="12" sm="" class="bg-surface pa-3 pa-sm-5 text-justify rounded-lg">
+                <v-icon color="red-lighten-3" class="text-h5 text-sm-h4">mdi-bug-outline</v-icon>
+                <p class="mt-5 font-weight-black text-subtitle-1 text-sm-h6">Malware-Infested Links</p>
                 <p class="mt-5">These links lead to downloads that install viruses, spyware, or ransomware on your
                     device. Avoid clicking on links from unknown sources, and ensure your antivirus software is
                     up-to-date.</p>
             </v-col>
         </v-row>
         <v-row class="gap my-10">
-            <v-col cols="12" sm="" class="bg-surface pa-5 rounded-lg">
-                <v-icon color="red-lighten-3" size="40">mdi-file-alert-outline</v-icon>
-                <p class="mt-5 font-weight-black text-h6">Phishing Links</p>
+            <v-col cols="12" sm="" class="bg-surface pa-3 pa-sm-5 text-justify rounded-lg">
+                <v-icon color="red-lighten-3" class="text-h5 text-sm-h4" >mdi-file-alert-outline</v-icon>
+                <p class="mt-5 font-weight-black text-subtitle-1 text-sm-h6">Phishing Links</p>
                 <p class="mt-5">Phishing links aim to steal your personal and financial information. These links are
                     often
                     disguised as legitimate emails or websites and ask for sensitive data. Always verify the source and
                     never provide personal information through untrusted links.</p>
             </v-col>
-            <v-col cols="12" sm="" class="bg-surface pa-5 rounded-lg">
-                <v-icon color="red-lighten-3" size="40">mdi-virus</v-icon>
-                <p class="mt-5 font-weight-black text-h6">Virus and Trojan Links</p>
+            <v-col cols="12" sm="" class="bg-surface pa-3 pa-sm-5 text-justify rounded-lg">
+                <v-icon color="red-lighten-3" class="text-h5 text-sm-h4" >mdi-virus</v-icon>
+                <p class="mt-5 font-weight-black text-subtitle-1 text-sm-h6">Virus and Trojan Links</p>
                 <p class="mt-5">These links often lead to downloads that infect your device with malicious software like
                     viruses or trojans. Be cautious when clicking links from unknown sources, especially those that
                     prompt
@@ -72,9 +72,9 @@
             </v-col>
         </v-row>
     </v-main>
-    <v-main class="mt-0 mt-sm-15 bg-surface pa-5 pt-15">
+    <v-main class="mt-0 mt-sm-15 bg-surface pa-3 pa-sm-5 text-justify pt-15">
         <div class="w-100 layout mx-auto mb-15">
-            <h1 class="text-center text-h5 text-sm-h4 font-weight-black mb-5 mb-sm-10">How to Stay One Step Ahead of
+            <h1 class="text-center text-h6 text-sm-h4 font-weight-black mb-5 mb-sm-10">How to Stay One Step Ahead of
                 Cyber Threats</h1>
             <ul class="custom-bullets">
                 <li>
@@ -235,6 +235,9 @@ const scanUrl = async() => {
     .bg {
         background: linear-gradient(180deg, #0A192F 70%, white 5%);
     }
+    .v-card {
+    box-shadow: 0px 0px 5px #b4c3d9;
+}
 }
 
 @media (max-width:380px) {
